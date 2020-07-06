ab8b145511f26e4788e496b04a63c02033e7d54e
Subhash <subhash.bose@egovernments.org>
2020-07-07 00:05:58 +0530

    Cloud functions for CRUD

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,51 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({credential: admin.credential.cert(serviceAccount), databaseURL: "https://playground-app-with-axios.firebaseio.com"});
+
+var db = admin.database();
+var ref = db.ref("mdms");
+
+//  Create and Deploy Your First Cloud Functions
+//  https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
- });
+});
+
+exports.saveData = functions.https.onRequest((request, response) => {
+  ref.child('designation').set([
+    {
+      code: "D02",
+      name: "Assistant02"
+    }, {
+      code: "D03",
+      name: "Assistant03"
+    }
+  ]);
+  response.send("Saved successfully!");
+});
+
+exports.updateData = functions.https.onRequest((request, response) => {
+  ref.child('designation').update({
+    "0": {
+      code: "D04",
+      name: "Assistant04"
+    }
+  });
+  response.send("Updated successfully!");
+});
+
+var ref = db.ref("mdms");
+
+exports.showData = functions.https.onRequest((request, response) => {
+  ref.on("value", function(snapshot) {
+    console.log(snapshot.val());
+    response.send(snapshot.val());
+  }, function(errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  });
+});
